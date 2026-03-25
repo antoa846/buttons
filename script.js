3076d38a84ad8f5ce7246f4dae6e78292391f337
antoa846 <antoa846@newschool.edu>
2026-03-25 21:37:21 +0000

    data attributes

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const modal = document.querySelector("#popup-modal");
-const openBtn = document.querySelector("#open-popup");
-const closeBtn = document.querySelector(".close-btn")[0];
+// const openBtn = document.querySelector("#open-popup");
+const closeBtn = document.getElementsByClassName("close-btn")[0];
 const body = document.querySelector("body");
+const modalContent = document.querySelector(".modal-content");
 
 // When the user clicks the button, open the modal
-openBtn.addEventListener('click', function(event) {
-  console.log("running")
-  modal.style.display = "block";
-})
-
+// openBtn.addEventListener('click', function(event) {
+//   console.log("running")
+//   modal.style.display = "block";
+// })
+document.querySelectorAll('button').forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    const rect = e.target.getBoundingClientRect();
+    modalContent.textContent = btn.dataset.text; 
+    modal.style.top = `${rect.bottom + window.scrollY + 8}px`;
+    modal.style.left = `${rect.left + window.scrollX}px`;
+    modal.style.display = 'block';
+    console.log(modal)
+  });
+});
 
 // When the user clicks on (x), close the modal
 closeBtn.onclick = function() {
