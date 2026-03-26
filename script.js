eb6a1241a55d1243f7ac5528029fd91668db2962
antoa846 <antoa846@newschool.edu>
2026-03-26 02:42:30 +0000

    a lot of stuff

--- script.js
+++ script.js
@@ -4,34 +4,37 @@ const closeBtn = document.getElementsByClassName("close-btn")[0];
 const body = document.querySelector("body");
 const modalContent = document.querySelector(".modal-content");
 
-// When the user clicks the button, open the modal
-// openBtn.addEventListener('click', function(event) {
-//   console.log("running")
-//   modal.style.display = "block";
-// })
+// document.querySelectorAll('button').forEach(btn => {
+//   btn.addEventListener('click', (e) => {
+//     const rect = e.target.getBoundingClientRect();
+//     modalContent.textContent = btn.dataset.text; 
+//     modal.style.top = `${rect.top + window.scrollY + -10}px`;
+//     modal.style.left = `${rect.left + window.scrollX + 100}px`;
+//     modal.style.display = 'block';
+//     console.log(modal)
+    
+//   });
+// });
+
 document.querySelectorAll('button').forEach(btn => {
   btn.addEventListener('click', (e) => {
     const rect = e.target.getBoundingClientRect();
-    modalContent.textContent = btn.dataset.text; 
-    modal.style.top = `${rect.bottom + window.scrollY + 8}px`;
-    modal.style.left = `${rect.left + window.scrollX}px`;
-    modal.style.display = 'block';
-    console.log(modal)
+ //toggle:
+    if (modal.style.display === 'block') {
+      modal.style.display = 'none';
+    } else {
+      modalContent.textContent = btn.dataset.text;
+      modal.style.top = `${rect.top + window.scrollY - 10}px`;
+      modal.style.left = `${rect.left + window.scrollX + 100}px`;
+      modal.style.display = 'block';
+    }
   });
 });
 
-// When the user clicks on (x), close the modal
-closeBtn.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-document.addEventListener("DOMContentLoaded", function(event) {
-  window.addEventListener('click', function(event) {
-    if (!modal.contains(event.target) && event.target !== openBtn) {
-        console.log("clicked");
-      modal.style.display = "none";
-    }
-  })
-})
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('button') && !modal.contains(e.target)) {
+    modal.style.display = 'none';
+    activeBtn = null;
+  }
+});
 
