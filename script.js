2382b2aa1435b0855e38324b99bcc2477b758f06
antoa846 <antoa846@newschool.edu>
2026-03-26 04:22:33 +0000

    final

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
 const modal = document.querySelector("#popup-modal");
-// const openBtn = document.querySelector("#open-popup");
 const closeBtn = document.getElementsByClassName("close-btn")[0];
 const body = document.querySelector("body");
 const modalContent = document.querySelector(".modal-content");
 
-// document.querySelectorAll('button').forEach(btn => {
-//   btn.addEventListener('click', (e) => {
-//     const rect = e.target.getBoundingClientRect();
-//     modalContent.textContent = btn.dataset.text; 
-//     modal.style.top = `${rect.top + window.scrollY + -10}px`;
-//     modal.style.left = `${rect.left + window.scrollX + 100}px`;
-//     modal.style.display = 'block';
-//     console.log(modal)
-    
-//   });
-// });
 
 document.querySelectorAll('button').forEach(btn => {
   btn.addEventListener('click', (e) => {
